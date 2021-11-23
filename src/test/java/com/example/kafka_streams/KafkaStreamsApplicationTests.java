fc065a307db7641a7619c4cced49531a77d3919c
Arseniy Mayachkin <senya158@gmail.com>
2021-11-23 17:37:41 +0300

    Add 3 stream variants

---
+++ src/test/java/com/example/kafka_streams/KafkaStreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafka_streams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
